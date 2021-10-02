95877b0276316e780d360f99a8b4b37c85c5c3a0
gregoire <bostgregoire@gmail.com>
2021-10-02 15:07:45 +0200

    Draw BMP in guiassets with UITLS function

--- OS/Core/guiassets.c
+++ OS/Core/guiassets.c
@@ -4,33 +4,7 @@ void loadScreen(char *msg) {
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
 
-    FIL fil;
-    BMP_t bmp;
-
-    BMP_zeroBMP(&bmp);
-
-    if(f_open(&fil, "sys/hourglass.bmp", FA_READ) != FR_OK) {
-        return;
-    }
-
-    BMP_setFile(&fil);
-
-    BMP_Err_t err;
-    err = BMP_parseFile(&bmp);
-
-    if(err) {
-        return;
-    }
-    
-    err = BMP_readData(&bmp);
-
-    if(err) {
-        return;
-    }
-
-    BMP_blit(&bmp, 85, 18);
-    BMP_release(&bmp);
-    f_close(&fil);
+    UTILS_drawBMP("sys/hourglass.bmp", 85, 18);
 
     ssd1306_SetCursor(25, 18);
     ssd1306_WriteString(msg, Font_7x10, White);
@@ -42,33 +16,7 @@ void splash(void) {
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
 
-    FIL fil;
-    BMP_t bmp;
-
-    BMP_zeroBMP(&bmp);
-
-    if(f_open(&fil, "sys/splash.bmp", FA_READ) != FR_OK) {
-        return;
-    }
-
-    BMP_setFile(&fil);
-
-    BMP_Err_t err;
-    err = BMP_parseFile(&bmp);
-
-    if(err) {
-        return;
-    }
-    
-    err = BMP_readData(&bmp);
-
-    if(err) {
-        return;
-    }
-
-    BMP_blit(&bmp, 0, 0);
-    BMP_release(&bmp);
-    f_close(&fil);
+    UTILS_drawBMP("sys/splash.bmp", 0, 0);
 
     ssd1306_SetCursor(0, 42);
     ssd1306_WriteString("(c) Gregoire BOST", Font_7x10, White);
